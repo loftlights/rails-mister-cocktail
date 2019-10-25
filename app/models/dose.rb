c3e1164441149461bdac035c039a68182d57e965
jeroen rutten <rutten.jr@gmail.com>
2019-10-25 10:39:11 +0800

    Day one of cocktail all finished

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,6 +1,6 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
-  validates :ingredient_id, presence: true
   validates :description, presence: true
+  validates :cocktail, uniqueness: { scope: :ingredient }
 end
